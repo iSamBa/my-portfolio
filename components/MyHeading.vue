95fccea90f327d6d1282185c9724a4d4355523bc
iSamBa <bahou.aissam@gmail.com>
2021-04-09 14:33:12 +0200

    add image animation for heading

--- components/MyHeading.vue
+++ components/MyHeading.vue
@@ -1,12 +1,12 @@
 <template>
   <section id="heading" class="box-inner ">
     <div class="content">
-      <div class="info">
-        <h3>Hello there 👋🏼 !!</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore similique repellendus beatae, animi reiciendis placeat saepe impedit ipsum, nesciunt, molestiae ad iure quasi quidem necessitatibus blanditiis? Doloribus amet non nostrum.</p>
-      </div>
       <div class="illustration">
         <img srcset="~/static/images/me.png" alt="profile picture">
+      </div>
+      <div class="information">
+        <h3>Hello there 👋🏼 !!</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore similique repellendus beatae, animi reiciendis placeat saepe impedit ipsum, nesciunt, molestiae ad iure quasi quidem necessitatibus blanditiis? Doloribus amet non nostrum.</p>
       </div>
     </div>
   </section>
@@ -17,7 +17,3 @@ export default {
   name: 'MyHeading'
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
