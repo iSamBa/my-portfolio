18e4e6cedecd164124edfcdd05c431c74eec819d
iSamBa <bahou.aissam@gmail.com>
2021-04-12 23:45:06 +0200

    implement loadmore function

--- components/ProjectsList.vue
+++ components/ProjectsList.vue
@@ -10,6 +10,9 @@
         :repo-url="project.repoURL"
         :web-url="project.webURL"
       />
+      <button v-if="loadMore" @click="loadMoreProjects">
+        LoadMore
+      </button>
     </div>
   </section>
 </template>
@@ -20,11 +23,38 @@ import ProjectItem from './ProjectItem.vue'
 export default {
   name: 'ProjectsList',
   components: { ProjectItem },
-  computed: {
-    projects () {
-      return Projects
+  data () {
+    return {
+      projects: [],
+      totalProjects: 0,
+      shownProjects: 0,
+      loadMore: false
+    }
+  },
+  mounted () {
+    this.getProjects()
+  },
+  methods: {
+    getProjects () {
+      this.totalProjects = Projects.length
+      if (Projects.length > 3) {
+        this.loadMore = true
+        this.shownProjects = 3
+        this.projects = Projects.slice(0, 3)
+      } else {
+        this.projects = Projects
+      }
+    },
+    loadMoreProjects () {
+      const additional = Math.min(this.totalProjects - this.shownProjects, 3)
+      this.shownProjects += additional
+      if (this.shownProjects === this.totalProjects) {
+        this.loadMore = false
+      }
+      this.projects = Projects.slice(0, this.shownProjects + additional)
     }
   }
+
 }
 </script>
 
