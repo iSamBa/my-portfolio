39166736a2d2cbbb0693f9506b468a10c35ec379
iSamBa <bahou.aissam@gmail.com>
2021-04-16 00:30:44 +0200

    add say hi button

--- components/MyHeading.vue
+++ components/MyHeading.vue
@@ -7,6 +7,9 @@
       <div class="information">
         <h2>My name is <strong>Aissam</strong></h2>
         <p>FULL STACK DEVELOPER</p>
+        <button class="btn btn-outline-light">
+          Say Hi !
+        </button>
       </div>
     </div>
     <span id="circle" />
